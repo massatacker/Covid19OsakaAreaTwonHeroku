076dd7ab0a29de96cadb3c554a923fb510dfdf41
仲野政賢 <masataka@MacProLate2013HS.local>
2020-05-04 22:22:11 +0900

    2020-0504-2222

--- app.py
+++ app.py
@@ -18,67 +18,84 @@ df_area_town = pd.read_excel(area_town_list_filename)
 df_area_twon_initial = df_area_town[ df_area_town['初期値']==1 ]
 df_area_twon_initial = df_area_twon_initial.set_index('区域')
 
+last_update = df_town_num_data.iloc[-1]['日付']
+
 osaka_color = '#364C97'
 light_color = '#1E90FF'
+comment_color = '#C0C0C0'
+update_color = '#777777'
 
-app = dash.Dash(__name__)
+title_str = '大阪府 区域別 市町村別 新型コロナウィルス陽性者数'
+discharge_comment = '※退院には死亡退院を含む'
+
+app = dash.Dash(__name__, meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1.0"}
+    ]
+)
+app.title = title_str
 
 server = app.server
 
 app.layout = html.Div(children=[
     html.Div([
-        html.Div(
-            html.H1('大阪府 区域別 市町村別 新型コロナウィルス陽性者数',
-            style = {'textAlign': 'center'})
-        ),
+        html.Div([
+            html.H2(title_str,
+            style = {'textAlign': 'center','margin-bottom':'0%'}),
+            html.Div([
+                #html.H4(f'{last_update.date()}更新',
+                html.H4('{}更新'.format(last_update.strftime('%Y/%m/%d')),
+                style = {'margin':'0%', 'color':update_color}),
+                html.A('出典:covid19-osaka.info',
+                href = 'https://covid19-osaka.info/'),
+            ],
+            style={'textAlign':'right','margin-bottom':'1%','margin-right':'2%'}),
+        ]),
         html.Div([
             dcc.Dropdown(
                 id = 'dropdown-for-area',
                 options = [{'label': i, 'value': i} for i in df_area_town['区域'].unique()],
                 searchable=False,
+                clearable=False,
                 value = '北大阪'
             ),
             dcc.Graph(
                 id="area-daily-graph",
+                #config = {'staticPlot': True},
             ),
             dcc.Graph(
                 id="area-total-graph",
-            )
-        ], style={
-            'display': 'inline-block',
-            'width': '49%',
-        }),
+                #config = {'staticPlot': True},
+            ),
+            html.H5(discharge_comment,
+            style = {'color':comment_color})
+        ],
+        style={"verticalAlign": "top"},
+        className='graph_box',
+        ),
         html.Div([
             dcc.Dropdown(
                 id = 'dropdown-for-town',
                 searchable=False,
+                clearable=False,
             ),
             dcc.Graph(
                 id="town-daily-graph",
+                #config = {'staticPlot': True},
             ),
             dcc.Graph(
                 id="town-total-graph",
-            )
-        ], style={
-            'display': 'inline-block',
-            'width': '49%',
-        })
+                #config = {'staticPlot': True},
+            ),
+            html.H5(discharge_comment,
+            style = {'color':comment_color})
+        ],
+        style={"verticalAlign": "top"},        
+        className='graph_box',
+        ),
     ])
 ])
 
-def create_BarChart(dff, area, sel_deta):
-    return {
-        'data': [go.Bar(
-            x = dff['日付'],
-            y = dff[sel_deta]
-        )],
-        'layout':{
-            'title': '{}'.format(sel_deta)
-        }
-    }
-
-
-def create_2BarChart(dff, bar1_data, bar1_name, bar2_data, bar2_name, title):
+def create_2BarChart(dff, area, bar1_data, bar1_name, bar2_data, bar2_name, title):
     return {
         'data': [go.Bar(
                     x = dff['日付'],
@@ -90,23 +107,26 @@ def create_2BarChart(dff, bar1_data, bar1_name, bar2_data, bar2_name, title):
                     y = dff[bar2_data],
                     name = bar2_name,
                     marker_color = light_color)
-        ],
+                  ],
         'layout':{
-            'title': '{}'.format(title),
+            'margin':{'l':30, 'r':20, 't':100, 'b':30},
+            'legend':{"x":0.85, "y":1.15},
+            'title': '{} {}'.format(area, title),
             'barmode':'group',
             'bargroupgap':0.0,
+            'xaxis':{'fixedrange':True, 'tickformat':'%_m/%-d'},
+            'yaxis':{'fixedrange':True},
             #'bargap':0.2,
-        }
+        },
     }
 
-def create_BarScatterChart(dff, bar_data, bar_name, scatter_data, scatter_name, title):
+def create_BarScatterChart(dff, area, bar_data, bar_name, scatter_data, scatter_name, title):
     return {
         'data': [go.Bar(
                     x = dff['日付'],
                     y = dff[bar_data],
                     name = bar_name,
                     marker_color = osaka_color),
-                    #marker = dict(color = osaka_color)),
                  go.Scatter(
                     x = dff['日付'],
                     y = dff[scatter_data],
@@ -114,7 +134,11 @@ def create_BarScatterChart(dff, bar_data, bar_name, scatter_data, scatter_name, 
                     line = dict(color = light_color, width = 4))
         ],
         'layout':{
-            'title': '{}'.format('{}'.format(title))
+            'margin':{'l':30, 'r':20, 't':100, 'b':30},
+            'legend':{"x":0.85, "y":1.15},
+            'title': '{} {}'.format(area, title),
+            'xaxis':{'fixedrange':True, 'tickformat':'%_m/%-d'},
+            'yaxis':{'fixedrange':True},
         }
     }
 
@@ -124,7 +148,7 @@ def create_BarScatterChart(dff, bar_data, bar_name, scatter_data, scatter_name, 
 )
 def update_graph(factor):
     dff = df_area_num_data[df_area_num_data['区域'] == factor]
-    return create_BarScatterChart(dff, '日別', '日別', '週平均', '週平均', '陽性者')
+    return create_BarScatterChart(dff, '日別', '日別', '週平均', '7日平均', '陽性者')
 
 @app.callback(
     dash.dependencies.Output('area-total-graph', 'figure'),
@@ -140,7 +164,7 @@ def update_graph(factor):
 )
 def update_graph(factor):
     dff = df_town_num_data[df_town_num_data['市町村'] == factor]
-    return create_BarScatterChart(dff, '日別', '日別', '週平均', '週平均', '陽性者')
+    return create_BarScatterChart(dff, '日別', '日別', '週平均', '7日平均', '陽性者')
 
 @app.callback(
     dash.dependencies.Output('town-total-graph', 'figure'),
