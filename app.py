8c7520b70477233adc2d43d1c9352cb0110d4677
Masataka Nakano <masataka_nakano@mac.com>
2022-02-23 20:14:03 +0900

    2022-0223-2014

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go  
 import pandas as pd 
-
+import numpy as np
+import datetime
 import os
 
 input_area_filename = 'Covid19_Osaka_Area2NumPopuData.xlsx'
@@ -21,12 +22,27 @@ df_area_twon_initial = df_area_twon_initial.set_index('地域')
 last_update = df_town_num_data.iloc[-1]['日付']
 
 osaka_color = '#364C97'
-light_color = '#1E90FF'
-comment_color = '#C0C0C0'
+light_color = '#1E90FF' # dodgerblue
+comment_color = '#C0C0C0' # silver
 update_color = '#777777'
+center_color = '#FFA500' # orange
+inc_color = '#FF4500' # orangered
+dec_color = '#32CD32' # limegreen
+center_text_color = '#D2691E' #chocolate
+inc_text_color = '#DC143C' # crimson #'#8B0000' # darkred
+dec_text_color = '#228B22' # forestgreen #'#006400' # darkgreen
 
 title_str = '大阪府 地域別 市町村別 新型コロナウイルス陽性者数'
 discharge_comment = '※退院には死亡退院を含む'
+
+inc_dec_start_date = datetime.datetime(2022, 1, 1)
+#inc_dec_start_date = datetime.datetime(2021, 7, 1)
+
+td_12h = datetime.timedelta(hours=12)
+graph_min_date = df_town_num_data.iloc[0]['日付'] + td_12h
+graph_max_date = last_update + td_12h
+graph_start_date = inc_dec_start_date + td_12h
+
 
 app = dash.Dash(__name__, meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1.0"},
@@ -53,6 +69,18 @@ app.layout = html.Div(children=[
             ],
             style={'textAlign':'right','margin-bottom':'0%','margin-right':'2%'}),
             html.Div([
+            html.Div([
+                dcc.RadioItems(
+                    id = 'radio_inc_ratio_persons',
+                    options=[
+                        {'label': '増加比率', 'value': 'inc_ratio'},
+                        {'label': '陽性者数', 'value': 'persons'},
+                    ],
+                    value='inc_ratio',
+                    labelStyle={'margin-left':'1%','margin-right':'1%'},
+                    ),
+            ],
+            style={'textAlign':'center','margin-bottom':'1%'}),
                 dcc.RadioItems(
                     id = 'radio_real_per_popu',
                     options=[
@@ -63,7 +91,19 @@ app.layout = html.Div(children=[
                     labelStyle={'margin-left':'1%','margin-right':'1%'},
                     ),
             ],
-            style={'textAlign':'center','margin-bottom':'1%'}),                
+            style={'textAlign':'center','margin-bottom':'1%'}),
+            html.Div([
+                dcc.RangeSlider(
+                    id = 'slider_range_date',
+                    min = graph_min_date.timestamp(),
+                    max = graph_max_date.timestamp(),
+                    value = [graph_start_date.timestamp(), graph_max_date.timestamp()],
+            ),
+            html.Div(
+                id='slider-container',
+                style={'textAlign':'center'})
+            ],
+            ),
         ]),
         html.Div([
             dcc.Dropdown(
@@ -71,16 +111,21 @@ app.layout = html.Div(children=[
                 options = [{'label': i, 'value': i} for i in df_area_town['地域'].unique()],
                 searchable=False,
                 clearable=False,
-                value = '豊能'
+                value = '全域',
+                #value = '豊能',
             ),
             dcc.Graph(
-                id="area-daily-graph",
+                id="area-graph",
                 #config = {'staticPlot': True},
             ),
-            dcc.Graph(
-                id="area-total-graph",
-                #config = {'staticPlot': True},
-            ),
+            #dcc.Graph(
+            #    id="area-daily-graph",
+            #    #config = {'staticPlot': True},
+            #),
+            #dcc.Graph(
+            #    id="area-total-graph",
+            #    #config = {'staticPlot': True},
+            #),
             #html.H5(discharge_comment,
             #style = {'color':comment_color})
         ],
@@ -94,13 +139,17 @@ app.layout = html.Div(children=[
                 clearable=False,
             ),
             dcc.Graph(
-                id="town-daily-graph",
+                id="town-graph",
                 #config = {'staticPlot': True},
             ),
-            dcc.Graph(
-                id="town-total-graph",
-                #config = {'staticPlot': True},
-            ),
+            #dcc.Graph(
+            #    id="town-daily-graph",
+            #    #config = {'staticPlot': True},
+            #),
+            #dcc.Graph(
+            #    id="town-total-graph",
+            #    #config = {'staticPlot': True},
+            #),
             #html.H5(discharge_comment,
             #style = {'color':comment_color})
         ],
@@ -154,14 +203,7 @@ def create_1BarChart(dff, area, bar1_data, bar1_name, title):
             'title':'{} {}'.format(area, title),
             'barmode':'group',
             'bargroupgap':0.0,
-            'xaxis':{'tickformat':'%_m/%-d', 
-                     'rangeslider':{'visible':True},
-                     'rangeselector':{
-                        'buttons':[{'count':1,'label':"1m",'step':"month",'stepmode':"backword"},
-                                   {'count':6,'label':"6m",'step':"month",'stepmode':"backword"},
-                                   {'count':1,'label':"YTD",'step':"year",'stepmode':"todate"},
-                                   {'count':1,'label':"1y",'step':"year",'stepmode':"backword"},
-                                   {'step':"all"}]}
+            'xaxis':{'fixedrange':True,'tickformat':'%_m/%-d', 
                     },
             'yaxis':{'fixedrange':True},
             #'bargap':0.2,
@@ -186,73 +228,263 @@ def create_BarScatterChart(dff, area, bar_data, bar_name, scatter_data, scatter_
             'legend':{"x":0.85, "y":1.15},
             'title':'{} {}'.format(area, title),
             'xaxis':{'tickformat':'%_m/%-d', 
-                     'rangeslider':{'visible':True},
-                     'rangeselector':{
-                        'buttons':[{'count':1,'label':"1m",'step':"month",'stepmode':"backword"},
-                                   {'count':6,'label':"6m",'step':"month",'stepmode':"backword"},
-                                   {'count':1,'label':"YTD",'step':"year",'stepmode':"todate"},
-                                   {'count':1,'label':"1y",'step':"year",'stepmode':"backword"},
-                                   {'step':"all"}]}
+                     'fixedrange':True,
                     },
             'yaxis':{'fixedrange':True},
         }
     }
 
+def create_ScatterIncRatioChart(dff, area, this_week_col, last_week_col, title):
+    # グラフの表示期間を限定する
+    # 開始：inc_dec_start_date
+    # 終了：最終更新日
+    this_week_data = dff[this_week_col]
+    last_week_data = dff[last_week_col]
+    this_week_data_max = this_week_data.max()
+    last_week_data_max = last_week_data.max()
+    # グラフスケールの最大値を計算する
+    if this_week_data_max > last_week_data_max:
+        r_max = this_week_data_max
+    else:
+        r_max = last_week_data_max
+    r_max = r_max*1.05
+    # センターラインデータを作成する
+    c_data = [0, r_max]
+    # 増加・減少ラインデータを作成する
+    x1_5_x_data = [0, r_max/1.5]
+    x2_0_x_data = [0, r_max/2.0]
+    x3_0_x_data = [0, r_max/3.0]
+    x5_0_x_data = [0, r_max/5.0]
+    x0_7_y_data = [0, r_max*(2.0/3.0)]
+    x0_5_y_data = [0, r_max*(1.0/2.0)]
+    target_line = dict(color = light_color, width = 3)
+    center_line = dict(color = center_color, width = 3, dash='dot')
+    inc_line = dict(color = inc_color, width = 2, dash='dot')
+    dec_line = dict(color = dec_color, width = 2, dash='dot')
+    last_update = dff.iloc[-1]['日付']
+    #last_update = df_town_num_data.iloc[-1]['日付']
+    if dff.iloc[-1]['先週累積'] == 0:
+        last_week_ratio = np.nan
+    else:
+        last_week_ratio = dff.iloc[-1]['週累積']/dff.iloc[-1]['先週累積']
+    annotations = [
+        dict(text = 'x1.5',
+             font = dict(color = inc_text_color),
+             x = x1_5_x_data[-1],
+             y = c_data[-1],
+             xref = 'x',
+             yref = 'y',
+             ax = 0,
+             ay = 5,
+             xanchor = 'left', 
+             yanchor = 'bottom',
+             showarrow = False
+        ),
+        dict(text = 'x2.0',
+             font = dict(color = inc_text_color),
+             x = x2_0_x_data[-1],
+             y = c_data[-1],
+             xref = 'x',
+             yref = 'y',
+             ax = 0,
+             ay = 5,
+             xanchor = 'left', 
+             yanchor = 'bottom',
+             showarrow = False
+        ),
+        dict(text = 'x3.0',
+             font = dict(color = inc_text_color),
+             x = x3_0_x_data[-1],
+             y = c_data[-1],
+             xref = 'x',
+             yref = 'y',
+             ax = 0,
+             ay = 5,
+             xanchor = 'left', 
+             yanchor = 'bottom',
+             showarrow = False
+        ),
+        dict(text = 'x5.0',
+             font = dict(color = inc_text_color),
+             x = x5_0_x_data[-1],
+             y = c_data[-1],
+             xref = 'x',
+             yref = 'y',
+             ax = 0,
+             ay = 5,
+             xanchor = 'left', 
+             yanchor = 'bottom',
+             showarrow = False
+        ),
+        dict(text = 'x2/3',
+             font = dict(color = dec_text_color),
+             x = c_data[-1],
+             y = x0_7_y_data[-1],
+             xref = 'x',
+             yref = 'y',
+             ax = 5, 
+             ay = 0, 
+             xanchor = 'left', 
+             showarrow = False
+        ),
+        dict(text = 'x1/2',
+             font = dict(color = dec_text_color),
+             x = c_data[-1],
+             y = x0_5_y_data[-1],
+             xref = 'x',
+             yref = 'y',
+             ax = 5, 
+             ay = 0, 
+             xanchor = 'left', 
+             showarrow = False
+        ),
+        dict(text = 'x1.0',
+             font = dict(color = center_text_color),
+             x = c_data[-1],
+             y = c_data[-1],
+             xref = 'x',
+             yref = 'y',
+             ax = 5, 
+             ay = 0, 
+             xanchor = 'left', 
+             showarrow = False
+        ),
+        dict(text = '{0}時点<br>前週増加比{1:.2f}'.format(last_update.strftime('%Y/%-m/%-d'),last_week_ratio),
+             font = dict(size = 14),
+             x = c_data[-1],
+             y = c_data[-1]*0.05,
+             xref = 'x',
+             yref = 'y',
+             align = 'left',
+             xanchor = 'right', 
+             yanchor = 'bottom',
+             showarrow = False
+        ),
+            ]
 
+    return {
+        'data': [go.Scatter(
+                    x = c_data,
+                    y = c_data,
+                    mode = 'lines',
+                    line = center_line,
+                    name = 'x1.0'),
+                 go.Scatter(
+                    x = x1_5_x_data,
+                    y = c_data,
+                    mode = 'lines',
+                    line = inc_line,
+                    name = 'x1.5'),
+                 go.Scatter(
+                    x = x2_0_x_data,
+                    y = c_data,
+                    mode = 'lines',
+                    line = inc_line,
+                    name = 'x2.0'),
+                 go.Scatter(
+                    x = x3_0_x_data,
+                    y = c_data,
+                    mode = 'lines',
+                    line = inc_line,
+                    name = 'x3.0'),
+                 go.Scatter(
+                    x = x5_0_x_data,
+                    y = c_data,
+                    line = inc_line,
+                    mode = 'lines',
+                    name = 'x5.0'),
+                 go.Scatter(
+                    x = c_data,
+                    y = x0_7_y_data,
+                    line = dec_line,
+                    mode = 'lines',
+                    name = 'x2/3'),
+                 go.Scatter(
+                    x = c_data,
+                    y = x0_5_y_data,
+                    line = dec_line,
+                    mode = 'lines',
+                    name = 'x1/2'),
+                 go.Scatter(
+                    x = last_week_data,
+                    y = this_week_data,
+                    text = dff['日付'],
+                    mode ='lines+markers+text',
+                    texttemplate='%{text|%-m/%-d}',
+                    textposition='bottom right',
+                    name = '前週増加比',
+                    line = target_line),
+                ],
+        'layout':{
+            'margin':{'l':50, 'r':50, 't':100, 'b':30},
+            #'margin':{'l':30, 'r':20, 't':100, 'b':30},
+            'showlegend':False,
+            'title':'{} {}'.format(area, title),
+            'xaxis':{
+                     #'fixedrange':True, 
+                     'range':(0, r_max),
+                     'title':{'text':'前週累計', 'font':{'size':12},'standoff':0},
+                     #'constrain':'domain',
+                     },
+            'yaxis':{
+                     #'fixedrange':True, 
+                     'range':(0, r_max),
+                     'title':{'text':'週累計', 'font':{'size':12},'standoff':0},
+                     'scaleanchor':'x',
+                     'scaleratio':1,
+                     },
+            'annotations':annotations
+        }
+    }
 
+def rs_value2datetime(rs_value):
+    return list(pd.to_datetime(rs_value, unit='s'))
 
 @app.callback(
-    dash.dependencies.Output('area-daily-graph', 'figure'),
+    dash.dependencies.Output('area-graph', 'figure'),
     [dash.dependencies.Input('dropdown-for-area', 'value'),
+     dash.dependencies.Input('radio_inc_ratio_persons', 'value'),
      dash.dependencies.Input('radio_real_per_popu', 'value'),
+     dash.dependencies.Input('slider_range_date', 'value'),
     ]
 )
-def update_graph(dropdown_value, radio_value):
+def update_graph(dropdown_value, radio_irp_value, radio_rpp_value, rs_value):
     dff = df_area_num_data[df_area_num_data['地域'] == dropdown_value]
-    if radio_value == 'real_persons':
-        return create_BarScatterChart(dff, dropdown_value, '日別', '日別', '週平均', '7日平均', '陽性者')
+    from_datetime, to_datetime = rs_value2datetime(rs_value)
+    dff = dff[(dff['日付']>=from_datetime)&(dff['日付'] <=to_datetime)]
+    if radio_irp_value == 'inc_ratio':
+        if radio_rpp_value == 'real_persons': 
+            return create_ScatterIncRatioChart(dff, dropdown_value, '週累積', '先週累積', '前週増加比')
+        else:
+            return create_ScatterIncRatioChart(dff, dropdown_value, '週累積人口10万人当たり', '先週累積人口10万人当たり', '前週増加比')
     else:
-        return create_1BarChart(dff, dropdown_value, '日別人口10万人当たり', '陽性', '陽性者')
+        if radio_rpp_value == 'real_persons':
+            return create_BarScatterChart(dff, dropdown_value, '日別', '日別', '週平均', '7日平均', '陽性者')
+        else:
+            return create_BarScatterChart(dff, dropdown_value, '日別人口10万人当たり', '日別', '週平均人口10万人当たり', '7日平均', '陽性者')
 
 @app.callback(
-    dash.dependencies.Output('area-total-graph', 'figure'),
-    [dash.dependencies.Input('dropdown-for-area', 'value'),
-     dash.dependencies.Input('radio_real_per_popu', 'value'),
-    ]
-)
-def update_graph(dropdown_value, radio_value):
-    dff = df_area_num_data[df_area_num_data['地域'] == dropdown_value]
-    if radio_value == 'real_persons':
-        return create_1BarChart(dff, dropdown_value, '累計', '陽性', '累計')
-    else:
-        return create_1BarChart(dff, dropdown_value, '週累積人口10万人当たり', '陽性', '週累積')
-
-@app.callback(
-    dash.dependencies.Output('town-daily-graph', 'figure'),
+    dash.dependencies.Output('town-graph', 'figure'),
     [dash.dependencies.Input('dropdown-for-town', 'value'),
+     dash.dependencies.Input('radio_inc_ratio_persons', 'value'),
      dash.dependencies.Input('radio_real_per_popu', 'value'),
+     dash.dependencies.Input('slider_range_date', 'value'),
     ]
 )
-def update_graph(dropdown_value, radio_value):
+def update_graph(dropdown_value, radio_irp_value, radio_rpp_value, rs_value):
     dff = df_town_num_data[df_town_num_data['市町村'] == dropdown_value]
-    if radio_value == 'real_persons':
-        return create_BarScatterChart(dff, dropdown_value, '日別', '日別', '週平均', '7日平均', '陽性者')
+    from_datetime, to_datetime = rs_value2datetime(rs_value)
+    dff = dff[(dff['日付']>=from_datetime)&(dff['日付'] <=to_datetime)]
+    if radio_irp_value == 'inc_ratio':
+        if radio_rpp_value == 'real_persons': 
+            return create_ScatterIncRatioChart(dff, dropdown_value, '週累積', '先週累積', '前週増加比')
+        else:
+            return create_ScatterIncRatioChart(dff, dropdown_value, '週累積人口10万人当たり', '先週累積人口10万人当たり', '前週増加比')
     else:
-        return create_1BarChart(dff, dropdown_value, '日別人口10万人当たり', '陽性', '陽性者')
-
-@app.callback(
-    dash.dependencies.Output('town-total-graph', 'figure'),
-    [dash.dependencies.Input('dropdown-for-town', 'value'),
-     dash.dependencies.Input('radio_real_per_popu', 'value'),
-    ]
-)
-def update_graph(dropdown_value, radio_value):
-    dff = df_town_num_data[df_town_num_data['市町村'] == dropdown_value]
-    if radio_value == 'real_persons':
-        return create_1BarChart(dff, dropdown_value, '累計', '陽性', '累計')
-    else:
-        return create_1BarChart(dff, dropdown_value, '週累積人口10万人当たり', '陽性', '週累積')
-
+        if radio_rpp_value == 'real_persons':
+            return create_BarScatterChart(dff, dropdown_value, '日別', '日別', '週平均', '7日平均', '陽性者')
+        else:
+            return create_BarScatterChart(dff, dropdown_value, '日別人口10万人当たり', '日別', '週平均人口10万人当たり', '7日平均', '陽性者')
 
 @app.callback(
     dash.dependencies.Output('dropdown-for-town', 'options'),
@@ -268,6 +500,14 @@ def update_dropdown_control(factor):
 )
 def update_dropdown_value(factor):
     return df_area_twon_initial.at[factor,'市町村']
+
+@app.callback(
+    dash.dependencies.Output('slider-container', 'children'),
+    [dash.dependencies.Input('slider_range_date', 'value')])
+def update_output(rs_value):
+    #if rs_value:
+    from_datetime, to_datetime = rs_value2datetime(rs_value)
+    return '{} 〜 {}'.format(from_datetime.strftime('%Y/%m/%d'), to_datetime.strftime('%Y/%m/%d'))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
