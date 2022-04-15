c775f2c1188bad7444fb49f8f59ceef40750d154
Masataka Nakano <masataka_nakano@mac.com>
2022-04-16 01:16:48 +0900

    2022-0416-0116

--- app.py
+++ app.py
@@ -35,7 +35,8 @@ dec_text_color = '#228B22' # forestgreen #'#006400' # darkgreen
 title_str = '大阪府 地域別 市町村別 新型コロナウイルス陽性者数'
 discharge_comment = '※退院には死亡退院を含む'
 
-inc_dec_start_date = datetime.datetime(2022, 1, 1)
+inc_dec_start_date = datetime.datetime(2022, 3, 20)
+#inc_dec_start_date = datetime.datetime(2022, 1, 1)
 #inc_dec_start_date = datetime.datetime(2021, 7, 1)
 
 td_12h = datetime.timedelta(hours=12)
